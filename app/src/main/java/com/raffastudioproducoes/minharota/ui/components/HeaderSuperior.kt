f29f87e431749d961b7bbc2cb381d1e6bc7e8893
raffastudioproducoes-cloud <raffastudioproducoes@gmail.com>
2026-07-07 13:16:50 -0300

    Update menu icon in HeaderSuperior component
    Replaced MenuIconTwoLines with a native Menu icon.

--- app/src/main/java/com/raffastudioproducoes/minharota/ui/components/HeaderSuperior.kt
+++ app/src/main/java/com/raffastudioproducoes/minharota/ui/components/HeaderSuperior.kt
@@ -70,7 +70,12 @@ fun HeaderSuperior(onDrawerClick: () -> Unit, drawerState: DrawerState? = null) 
                         tint = textColor
                     )
                 } else {
-                    MenuIconTwoLines()
+                    // Ícone oficial nativo que NÃO bloqueia o clique!
+                    Icon(
+                        imageVector = androidx.compose.material.icons.Icons.Rounded.Menu,
+                        contentDescription = "Abrir Menu",
+                        tint = textColor
+                    )
                 }
             }
         },
